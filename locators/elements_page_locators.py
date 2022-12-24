779d231cf73ea9c9ab5c04bfdaad6805bc97f25d
Alexey Tyukhteev <79310074+TyukhteevAM@users.noreply.github.com>
2022-12-24 18:56:56 +0300

    update elements test added tests radio button

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -24,6 +24,13 @@ class CheckBoxPageLocators:
     TITLE_ITEM = ".//ancestor::span[@class='rct-text']"
     OUTPUT_RESULT = (By.CSS_SELECTOR, "span[class='text-success']")
 
+class RadioButtonPageLocators:
+    YES_RADIOBUTTON = (By.CSS_SELECTOR, 'label[class^="custom-control"][for="yesRadio"]')
+    IMPRESSIVE_RADIOBUTTON = (By.CSS_SELECTOR, 'label[class^="custom-control"][for="impressiveRadio"]')
+    NO_RADIOBUTTON = (By.CSS_SELECTOR, 'label[class^="custom-control"][for="noRadio"]')
+    OUTPUT_RESULT = (By.CSS_SELECTOR, 'p span[class="text-success"]')
+
+
 
 
 
