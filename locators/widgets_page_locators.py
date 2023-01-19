d8183b7eb7a293fbe4d28c1f5a5bf914bd14dacc
Alexey Tyukhteev <79310074+TyukhteevAM@users.noreply.github.com>
2023-01-19 14:06:38 +0300

    added class:  - TestToolTipsPage added test:  - test_tool_tips update:  -base_page

--- locators/widgets_page_locators.py
+++ locators/widgets_page_locators.py
@@ -49,3 +49,23 @@ class TabsPageLocators:
     USE_TAB_TEXT = (By.XPATH, '//div[@id="demo-tabpane-use"]/p')
     MORE_TAB = (By.CSS_SELECTOR, 'a[id="demo-tab-more"]')
     MORE_TAB_TEXT = (By.XPATH, '//a[@id="demo-tabpane-more"]/p')
+
+class ToolTipsPageLocators:
+    BUTTON = (By.CSS_SELECTOR, 'button[id="toolTipButton"]')
+    TOOL_TIP_BUTTON = (By.CSS_SELECTOR, 'button[aria-describedby="buttonToolTip"]')
+
+    FIELD = (By.CSS_SELECTOR, 'input[id="toolTipTextField"]')
+    TOOL_TIP_FIELD = (By.CSS_SELECTOR, 'input[aria-describedby="textFieldToolTip"]')
+
+    CONTRARY_LINK = (By.XPATH, '//*[.="Contrary"]')
+    TOOL_TIP_CONTRARY = (By.CSS_SELECTOR, 'a[aria-describedby="contraryTexToolTip"]')
+
+    SECTION_LINK = (By.XPATH, '//*[.="1.10.32"]')
+    TOOL_TIP_SECTION = (By.CSS_SELECTOR, 'a[aria-describedby="sectionToolTip"]')
+
+    TOOL_TIPS_INNERS = (By.CSS_SELECTOR, 'div[class="tooltip-inner"]')
+
+
+
+
+
--- pages/base_page.py
+++ pages/base_page.py
@@ -49,7 +49,10 @@ class BasePage:
         action.drag_and_drop_by_offset(element, x_coord, y_coord)
         action.perform()
 
-
+    def action_move_to_element(self, element):
+        action = ActionChains(self.driver)
+        action.move_to_element(element)
+        action.perform()
 
     def footer_remove(self):
         self.driver.execute_script("document.getElementsByTagName('footer')[0].remove();")
