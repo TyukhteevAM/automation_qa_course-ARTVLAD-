48b052968d97174b230ef49d8e9843f05b419cb4
Alexey Tyukhteev <79310074+TyukhteevAM@users.noreply.github.com>
2023-01-27 14:52:23 +0300

    added class:  - TestResizablePage: added test:  - test_resizable

--- locators/interaction_page_locators.py
+++ locators/interaction_page_locators.py
@@ -19,3 +19,13 @@ class SelectablePageLocators:
     GRID_ITEM = (By.CSS_SELECTOR, 'div[id="gridContainer"]  li[class="list-group-item list-group-item-action"]')
     GRID_ITEM_ACTIVE = (
         By.CSS_SELECTOR, 'div[id="gridContainer"]  li[class="list-group-item active list-group-item-action"]')
+
+
+class ResizablePageLocators:
+    RESIZABLE_BOX_HANDLE = (By.CSS_SELECTOR,
+                            'div[class="constraint-area"] span[class="react-resizable-handle '
+                            'react-resizable-handle-se"]')
+    RESIZABLE_BOX = (By.XPATH, '//div[@id="resizableBoxWithRestriction"]')
+    RESIZABLE_HANDLE = (By.CSS_SELECTOR,
+                        'div[id="resizable"] span[class="react-resizable-handle react-resizable-handle-se"]')
+    RESIZABLE = (By.XPATH, '//div[@id="resizable"]')
